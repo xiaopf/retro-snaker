daa0e8476fd4674e86949c3203441d03a3b33161
xiaopf <326258842@qq.com>
2017-03-14 15:34:21 +0800

    AI第一步完成可以在吃食
    还是一条盲目地的蛇，不顾一切的吃东西！

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-var s=10; //网格的数量
+var s=60; //网格的数量
+
 
 //初始随机产生蛇头的位置
 var x=Math.floor(Math.random()*s),
@@ -16,6 +17,7 @@ var u=d=l=r=false; //防止蛇头回头变量
 
 var snaker=new Array(0);  //设置蛇身的数组
     snaker.push([x,y]);  //将初始化蛇头推入数组
+var path=[];
 
 
 $(document).ready(function(){
@@ -23,7 +25,7 @@ $(document).ready(function(){
     for (var i = 0; i < s; i++) {
         $("table")[0].innerHTML+='<tr class="'+'r'+i+'"></tr>';
         for (var j = 0; j < s; j++) {
-            $("tr")[i].innerHTML+='<td class="'+'d'+j+' smallBox">'+j+'</td>';
+            $("tr")[i].innerHTML+='<td class="'+'d'+j+' smallBox"></td>';
         }   
     }
     
@@ -37,6 +39,7 @@ $(document).ready(function(){
       snaker.push([x,y]);
       drawBody();
       random();
+      searchPath(snaker,x,y);
     }
     
     //画蛇 
@@ -95,134 +98,145 @@ $(document).ready(function(){
     // 初始化画蛇和食物
     drawBody();
     random();
+    searchPath(snaker,x,y);
      console.log(rX,rY)
      console.log(x,y)
 
-    $('#ai').click(function(){
 
-        var path=[];
-        var arrWall=[[5,0],[5,1],[5,3],[5,4],[5,5],[5,6],[5,7],[5,8],[5,9]];
-drawBody(0,arrWall,'yellow');
+
+    $('#ai').click(function(){
+      console.log(path)
+      move();    
+    })
+
+
+    function searchPath(arr,x,y){
+
         var openList=[];
-        var closeList=[[5,0],[5,1],[5,3],[5,4],[5,5],[5,6],[5,7],[5,8],[5,9]];
-        var G=1;
-        var mX=x,mY=y;
-       
+        var closeList=[].concat(arr);
+        var mX=x,mY=y; 
         closeList.push([mX,mY]);
  
+        while(!(mX==rX&&mY==rY)){
+
+            if(mX-1!=-1&&mX-1!=s&&!isInArr(closeList,mX-1,mY)&&!isInArr(openList,mX-1,mY)){
+              openList.push([mX-1,mY,getF([mX-1,mY]),mX,mY,]);
+            };
+
+            if(mY+1!=-1&&mY+1!=s&&!isInArr(closeList,mX,mY+1)&&!isInArr(openList,mX,mY+1)){
+              openList.push([mX,mY+1,getF([mX,mY+1]),mX,mY,]);
+            };
+
+            if(mX+1!=-1&&mX+1!=s&&!isInArr(closeList,mX+1,mY)&&!isInArr(openList,mX+1,mY)){
+              openList.push([mX+1,mY,getF([mX+1,mY]),mX,mY,]);
+            };
+
+            if(mY-1!=-1&&mY-1!=s&&!isInArr(closeList,mX,mY-1)&&!isInArr(openList,mX,mY-1)){
+              openList.push([mX,mY-1,getF([mX,mY-1]),mX,mY,]);
+            };
+            
+            function getF(arr){
+                var G=Math.abs(arr[0]-x)+Math.abs(arr[1]-y);
+                var H=Math.abs(arr[0]-rX)+Math.abs(arr[1]-rY);
+                return [G,H,G+H];
+            }
+        
+            openList.sort(function(a,b){
+              return b[2][2]-a[2][2];
+            });
+
+            var oLen=openList.length;
+
+            // console.log(openList,oLen);
+
+            if(oLen>1&&(openList[oLen-1][2][2]==openList[oLen-2][2][2])){
+
+                var openListSlice=new Array(0);
+                openListSlice=openList.slice(oLen-2);
+
+                // console.log(openListSlice);
+
+                var olLen=openListSlice.length;
+
+                openListSlice.sort(function(a,b){
+                   return b[2][1]-a[2][1];
+                });
+
+                var openOut=openListSlice.pop(); 
+
+                openList.splice(openList.indexOf(openOut),1)
+
+            }else{
+                var openOut=openList.pop(); 
+            }
+
+            // drawBody([openOut],'orange');
+            closeList.push(openOut);
+            mX=openOut[0];
+            mY=openOut[1];
+        };
+
+        var len=closeList.length;      
+        path.unshift([rX,rY]);   
+        path.unshift([closeList[len-1][3],closeList[len-1][4]]);
+
+        var a=path[0][0];
+        var b=path[0][1];
+        var ss=0;
+
+        while(!(a==x&&b==y)){
+
+            for (var i = len-1-ss; i >=0; i--) {
+
+              if(closeList[i][0]==a&&closeList[i][1]==b){
+                  path.unshift([closeList[i][3],closeList[i][4]]);
+                  ss=len-i;
+                  break;
+              }
+            }
+            a=path[0][0];
+            b=path[0][1];
+        }
+        
+        console.log(path)
+        path.shift();
+
+        if(path[1]){
+          return true;
+        }
+        // drawBody(path.slice(1,path.length-1),'grey');
 
 
-        function isInCloseList(aX,aY){
-          var k;
-          closeList.forEach(function(item){
+
+        // function drawBody(arr,color){
+        //     var i=0;
+        //     var arrLen=arr.length;
+        //     var timer=setInterval(function(){
+
+        //         if(i<arrLen){
+        //            draw(arr[i][0],arr[i][1],color); 
+        //         }else{
+        //            clearInterval(timer)
+        //         }
+
+        //         i++;
+
+        //     },100);
+        // }  
+
+        function isInArr(arr,aX,aY){
+          var k=false;
+          arr.forEach(function(item){
               if(item[0]==aX&&item[1]==aY){
                   k=true;
               }
           });
           return k;
-        }
+        } 
+}
+       
 
-
-        while(!(mX==rX&&mY==rY)){
-
-            
-
-            if(mX-1!=-1&&mX-1!=s&&!isInCloseList(mX-1,mY)){openList.push(     [mX-1,mY,getF([mX-1,mY],G),mX,mY])        ;};
-              if(mY+1!=-1&&mY+1!=s&&!isInCloseList(mX,mY+1)){openList.push(     [mX,mY+1,getF([mX,mY+1],G),mX,mY])        ;};
-                if(mX+1!=-1&&mX+1!=s&&!isInCloseList(mX+1,mY)){openList.push(     [mX+1,mY,getF([mX+1,mY],G),mX,mY])        ;};
-                  if(mY-1!=-1&&mY-1!=s&&!isInCloseList(mX,mY-1)){openList.push(     [mX,mY-1,getF([mX,mY-1],G),mX,mY])        ;};
-            
-          
-            function getF(arr,G){
-                return Math.abs(arr[0]-rX)+Math.abs(arr[1]-rY)+G;
-            }
-
-            openList.sort(function(a,b){
-              return b[2]-a[2];
-            });
-
-            var openOut=openList.pop();
-
-            closeList.push(openOut);
-            
-            mX=openOut[0];
-            mY=openOut[1];
-
-            G++;
-
-        };
-
-        console.log(closeList)
-        var len=closeList.length;
-           
-        path.unshift([rX,rY]);   
-        path.unshift([closeList[len-1][3],closeList[len-1][4]]);
-
-        console.log(path)
-
-        var a=path[0][0];
-        var b=path[0][1];
-
-        console.log(a,b);
-
-        while(!(a==x&&b==y)){
-
-
-
-          for (var i = len-1; i >=0; i--) {
-
-              if(closeList[i][0]==a&&closeList[i][1]==b){
-
-                  path.unshift([closeList[i][3],closeList[i][4]]);
-
-                  break;
-
-              }
-          }
-
-          a=path[0][0];
-          b=path[0][1];
-        }
-
-
-
-
-        
-
-
-        drawBody(0,path,'grey');
-
-        function drawBody(n,arr,c){
-            for(var i=n;i<arr.length;i++){
-                draw(arr[i][0],arr[i][1],c);           
-            }
-        }      
-
-  
-      
-          
-        
-      
-
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    })
+ 
 
 
 
@@ -272,41 +286,69 @@ drawBody(0,arrWall,'yellow');
 
     function move(){
 
-        setTimeout(function(){time++},200);
+        // setTimeout(function(){time++},200);
 
         clearInterval(timer);
 
         timer=setInterval(function(){
 
-            switch(dir){
-                case 'u':
-                  x--;
-                  break;
-                case 'd':
-                  x++;
-                  break;
-                case 'l':
-                  y--;
-                  break;
-                case 'r':
-                  y++;
-                  break;
-            };
-            console.log('AUTO')
-            if(x==s||y==s||x==-1||y==-1||isInBody(x,y)){
-                alert('game over!')
-                init();
-                clearInterval(timer);
-            }else if(x==rX&&y==rY){
-                snaker.unshift([x,y]);
+
+
+            
+            // console.log(path)
+            var p=path.shift();
+
+            console.log(p)
+
+            if(!p){
+
+                  
+
+                  do{
+                      var  a=Math.floor(Math.random()*s);
+                      var  b=Math.floor(Math.random()*s); 
+                  }while(isInBody(aX,aY)&&searchPath(snaker,a,b));
+
+                  searchPath(snaker,a,b);
+                  
+
+            }else{
+              
+            }
+            
+
+            // switch(dir){
+            //     case 'u':
+            //       x--;
+            //       break;
+            //     case 'd':
+            //       x++;
+            //       break;
+            //     case 'l':
+            //       y--;
+            //       break;
+            //     case 'r':
+            //       y++;
+            //       break;
+            // };
+
+            // if(x==s||y==s||x==-1||y==-1||isInBody(x,y)){
+            //     alert('game over!')
+            //     init();
+            //     clearInterval(timer);
+            // }else 
+
+            if(p[0]==rX&&p[1]==rY){
+                snaker.unshift(p);
                 random();
+                searchPath(snaker,snaker[0][0],snaker[0][1])
                 drawBody(); 
             }else{
                 unDraw(snaker[snaker.length-1][0],snaker[snaker.length-1][1]);
                 snaker.pop();
-                snaker.unshift([x,y])
+                snaker.unshift(p)
                 drawBody();        
             };  
-        },200);
+        },100);
      }
 })
